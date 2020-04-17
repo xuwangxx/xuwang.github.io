f4be673daba7b007d466be7515b9fff08a4bbd39
xuwang <xuwang@duia.com>
2020-04-17 11:35:04 +0800

    sasfdgfdh

---
+++ precache-manifest.1bfb031e2c373c65f8b6bf5d335aecd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2328c47c9c2919c751d0bc61f951a32f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1f50e3202a69c012633",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4d5b45786fb680e8fa42",
+    "url": "/static/js/2.0344d207.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0344d207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f50e3202a69c012633",
+    "url": "/static/js/main.e7f7ee6a.chunk.js"
+  },
+  {
+    "revision": "3e9423373ecc8dfe0015",
+    "url": "/static/js/runtime-main.e2f55b13.js"
+  },
+  {
+    "revision": "33c0e4735c2ed68d13d1aa381ca34a90",
+    "url": "/static/media/error.33c0e473.png"
+  }
+]);
